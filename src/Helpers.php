64dc7147a522be1d7f1f949008771040776a2936
Shegun Babs <shegun.babs@gmail.com>
2019-05-29 16:59:31 +0100

    helper start

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,1 +1,13 @@
 <?php
+
+if ( !function_exists('call_url') )
+{
+    function call_url($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+        curl_close($ch);
+    }
+}
